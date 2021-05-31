7ce298381383b7aa9ee14d3afdf0c525ca3f2431
Warnner Sinotti <warnner13@gmail.com>
2021-05-31 15:00:47 -0300

    final frontend

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,20 +1,55 @@
-import React, {  useState } from "react";
+import React, { useState, useMemo } from "react";
+import api from "../../services/api";
 
+import camera from "../../assets/camera.svg";
 
+import "./styles.css";
 
-export default function New() {
+export default function New({history}) {
   const [company, setCompany] = useState("");
-const [techs, setTechs] = useState("");
-const [price, setPrice] = useState("");
+  const [techs, setTechs] = useState("");
+  const [price, setPrice] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
 
-  function handleSubmit() {}
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const data = new FormData();
+    const user_id = localStorage.getItem("user");
+
+    data.append("company", company);
+    data.append("techs", techs);
+    data.append("price", price);
+    data.append("thumbnail", thumbnail);
+
+    await api.post("/spots", data, {
+      headers: { user_id },
+    });
+
+    history.push("/dashboards");
+  }
 
   return (
     <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? "has-thumbnail" : ""}
+      >
+        <input
+          type="file"
+          onChange={(event) => setThumbnail(event.target.files[0])}
+        />
+        <img src={camera} alt="select img" />
+      </label>
       <label htmlFor="company">EMPRESA</label>
       <input
         id="company"
-        placeholder="sua empresa incrível"
+        placeholder="Sua empresa incrível"
         value={company}
         onChange={(event) => setCompany(event.target.value)}
       />
@@ -38,9 +73,10 @@ const [price, setPrice] = useState("");
         value={price}
         onChange={(event) => setPrice(event.target.value)}
       />
-     
 
-      <button type="submit" className="loginbtn">Cadastrar</button>
+      <button type="submit" className="loginbtn">
+        Cadastrar
+      </button>
     </form>
   );
 }
